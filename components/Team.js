17a7261b6802d80636942001a6ee30c99216f448
Mazin Ibrahim <me@dmzda.co.uk>
2022-06-05 21:00:16 +0100

    Tweak styling

--- components/Team.js
+++ components/Team.js
@@ -1,22 +1,23 @@
 const Team = ({ team }) => {
   return (
-    <li className="m-1">
-      <span className="border-y-2 border-l-2 border-black rounded-l-md inline-block p-1">
+    <li className="m-1 text-center">
+      <span className="border-y-2 border-l-2 border-black rounded-l-md inline-block py-0.5 px-2">
         {team.position}
       </span>
-      <span className="border-y-2 border-l-2 border-black inline-block p-1">
-        {team.team.name}
+      <span className="border-y-2 border-l-2 border-black inline-block py-0.5 px-2">
+        {team.team.shortName}
       </span>
-      <span className="border-y-2 border-l-2 border-black inline-block p-1">
+      <span className="border-y-2 border-l-2 border-black inline-block py-0.5 px-2 bg-green-200">
         {team.won}
       </span>
-      <span className="border-y-2 border-l-2 border-black inline-block p-1">
+      <span className="border-y-2 border-l-2 border-black inline-block py-0.5 px-2 bg-yellow-100">
         {team.draw}
       </span>
-      <span className="border-y-2 border-l-2 border-black inline-block p-1">
+      <span className="border-y-2 border-l-2 border-black inline-block py-0.5 px-2 bg-red-200">
         {team.lost}
       </span>
-      <span className="border-2 border-black inline-block p-1 rounded-r-md">
+      <span className="border-2 border-black inline-block py-0.5 px-2 rounded-r-md">
+        {team.goalDifference > 0 ? "+" : ""}
         {team.goalDifference}
       </span>
     </li>
